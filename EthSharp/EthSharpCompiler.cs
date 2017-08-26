378f57625214fe153dee77b2c4fcb4e82029890a
Jandrews <jordandrews@hotmail.com>
2017-08-26 11:19:13 -0500

    Breaks but shows ideas

--- EthSharp/EthSharpCompiler.cs
+++ EthSharp/EthSharpCompiler.cs
@@ -20,7 +20,14 @@ namespace EthSharp
 
         public Assembly.EthSharpAssembly Create(SyntaxTree root)
         {
+            //for now just assume one class
+            var rootClass = root.GetRoot().ChildNodes().FirstOrDefault(); // cast as class type
             InitializeContext();
+
+            foreach (var classNode in rootClass.Members)
+            {
+                
+            }
 
             throw new NotImplementedException();
         }
@@ -33,9 +40,15 @@ namespace EthSharp
 
             //can use freeMemoryPointer const here
 
+            // Compiled Contracts go here in solidity
+
             Context.Append(64 + 32);
             Context.Append(64);
             Context.Append(EvmInstruction.MSTORE);
+
+            // Registering state variables goes here in solidity. Not sure if important here
+
+            // Also resets visiting of node. Unsure how important
         }
     }
 }
